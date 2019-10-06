324ecf486e4949e5f284669ed36d3fe3bff1bba6
jfcarocota <jfcarocota@gmail.com>
2019-10-05 18:15:47 -0700

    publish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*const HelloWorld = (props)=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.message}
     </div>
   );
+}*/
+
+class HelloWorld extends React.Component{
+
+  state = {
+    show: true
+  }
+
+  toogleShow = () =>{
+    this.setState({show: !this.state.show});
+  }
+
+  render(){
+    if(this.state.show){
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.message}
+          <button onClick={this.toogleShow}>Toggle show</button>
+        </div>
+      )
+    }else{
+      return <h1>
+        Not elemnts yet
+        <button onClick={this.toogleShow}>ToogleShow</button>
+      </h1>
+    }
+  }
+
 }
+
+const App = ()=> <div>This is my component: <HelloWorld message="hello world" subtitle="lorem ipsum"/></div>
+
 
 export default App;
